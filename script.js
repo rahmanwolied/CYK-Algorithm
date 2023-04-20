c473e942f160a81d1027b1a6756e69b7dfe60575
rahmanwolied <mosheur.r.wolied@gmail.com>
2023-04-20 19:33:23 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+	var rules = {};
+	var rows = document.querySelectorAll('.row');
+	rows.forEach((row) => {
+		var variable = row.querySelector('.variable');
+		var values = row.querySelectorAll('.value');
+	});
+});
